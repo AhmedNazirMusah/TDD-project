9904e077019442e3160589bf57b3bc2bd93ff7e6
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-05 13:45:26 +0000

    Add test for factorial method

---
+++ spec/factorial_spec.rb
@@ -0,0 +1,36 @@
+require './solver'
+
+describe Solver do
+  context 'When testing the Solver class' do
+    solver = Solver.new
+
+    it 'solver.factorial should return the factorial of the number' do
+      number = 5
+      output = solver.factorial(number)
+      expect(output).to eq 120
+    end
+
+    it 'solver.factorial should return the factorial of the number' do
+      number = 4
+      output = solver.factorial(number)
+      expect(output).to eq 24
+    end
+
+    it 'solver.factorial should return the factorial of the number' do
+      number = 0
+      output = solver.factorial(number)
+      expect(output).to eq 1
+    end
+
+    it 'solver.factorial should return the factorial of the number' do
+      number = 1
+      output = solver.factorial(number)
+      expect(output).to eq 1
+    end
+
+    it 'solver.factorial should return the factorial of the number' do
+      number = -2
+      expect { solver.factorial(number) }.to raise_error(Error)
+    end
+  end
+end
